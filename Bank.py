e04f60cdb7c969db446b8cc72d225dfd0750705c
TensaZangetsu0 <136436906+TensaZangetsu0@users.noreply.github.com>
2023-09-23 23:46:03 +0530

    Bank.py

--- Bank.py
+++ Bank.py
@@ -1,7 +1,7 @@
 class Atm:
   def __init__(self):
-    self.pin=""
-    self.balance=0
+    self.__pin=""
+    self.__balance=0
     print("Welcome To The Atm")
 
     self.menu()
@@ -28,16 +28,16 @@ class Atm:
       self.exit()
 
   def create_pin(self):
-    self.pin=input("Create Your pin: ")
+    self.__pin=input("Create Your pin: ")
     print("Pin created sucessfully")
 
     self.menu()
 
   def deposit(self):
     temp=input("Enter your pin")
-    if temp==self.pin:
+    if temp==self.__pin:
       amount=int(input("Enter the Amount"))
-      self.balance=self.balance + amount 
+      self.__balance=self.__balance + amount 
       print("Deposited Sucessfully")
     else:
       print("Incorrect pin")
@@ -46,10 +46,10 @@ class Atm:
 
   def withdrawl(self):
     temp=input("Enter your pin")
-    if temp==self.pin:
+    if temp==self.__pin:
       amount=int(input("Enter the Amount"))
-      if amount <= self.balance:
-        self.balance=self.balance-amount
+      if amount <= self.__balance:
+        self.__balance=self.__balance-amount
         print("Withdrawl sucessfully",amount)
       else:
         print("Insufficient amounts")
@@ -60,8 +60,8 @@ class Atm:
 
   def check_balance(self):
     temp=input("Enter your pin")
-    if temp==self.pin:
-      print("Your Remaining balance",self.balance)
+    if temp==self.__pin:
+      print("Your Remaining balance",self.__balance)
     else:
       print("Incorrect pin")  
 
